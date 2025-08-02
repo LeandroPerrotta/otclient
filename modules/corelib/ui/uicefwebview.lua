daeb166d7d4891060323c284b61d36d09a2bec03
Leandro <leandro_perrotta@hotmail.com>
2025-08-01 22:08:04 -0300

    Improving HTML components loader (unfinished)

---
+++ modules/corelib/ui/uicefwebview.lua
@@ -0,0 +1,44 @@
+UICEFWebView = extends(UIWidget, "UICEFWebView")
+
+function UICEFWebView.create()
+    local webView = UICEFWebView.internalCreate()
+
+    return webView
+end
+
+function UICEFWebView:loadComponent(component_name, component_path)
+
+    local css_tag_template = '<link rel="stylesheet" type="text/css" href="{{ css_path }}">'
+    local js_tag_template = '<script type="text/javascript" src="{{ js_path }}"></script>'
+
+    local resolvePathFn = g_resources.resolvePath
+
+    local css_file_list = { resolvePathFn('../webviews/default.css'), resolvePathFn(component_path .. '/' .. component_name .. '.css')}
+    local js_file_list = { resolvePathFn('../webviews/default.js'), resolvePathFn(component_path .. '/' .. component_name .. '.js')}
+
+    local absolutePathFn = g_resources.getRealPath
+
+    local css_styles = ''
+    local js_scripts = ''
+
+    for _, css_file in ipairs(css_file_list) do
+        local css_path = absolutePathFn(css_file)
+        local css_tag = css_tag_template:gsub('{{ css_path }}', css_path)
+        css_styles = css_styles .. css_tag
+    end
+    
+    for _, js_file in ipairs(js_file_list) do
+        local js_path = absolutePathFn(js_file)
+        local js_tag = js_tag_template:gsub('{{ js_path }}', js_path)
+        js_scripts = js_scripts .. js_tag
+    end
+
+    local html = g_resources.readFileContents('webviews/index.template.html')
+    local body_content = g_resources.readFileContents(resolvePathFn(component_path .. '/' .. component_name .. '.html'))
+
+    html = html:gsub('{{ css_styles }}', css_styles)
+    html = html:gsub('{{ js_scripts }}', js_scripts)
+    html = html:gsub('{{ body_content }}', body_content)
+
+    self:loadHtml(html)
+end
