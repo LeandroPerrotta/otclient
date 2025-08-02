46d89c30aa05362c6ba297a64e9ab97a21156cae
Leandro <leandro_perrotta@hotmail.com>
2025-08-01 22:30:50 -0300

    Improving HTML components loader (unfinished)

--- modules/corelib/ui/uicefwebview.lua
+++ modules/corelib/ui/uicefwebview.lua
@@ -1,11 +1,3 @@
-UICEFWebView = extends(UIWidget, "UICEFWebView")
-
-function UICEFWebView.create()
-    local webView = UICEFWebView.internalCreate()
-
-    return webView
-end
-
 function UICEFWebView:loadComponent(component_name, component_path)
 
     local css_tag_template = '<link rel="stylesheet" type="text/css" href="{{ css_path }}">'
@@ -13,8 +5,8 @@ function UICEFWebView:loadComponent(component_name, component_path)
 
     local resolvePathFn = g_resources.resolvePath
 
-    local css_file_list = { resolvePathFn('../webviews/default.css'), resolvePathFn(component_path .. '/' .. component_name .. '.css')}
-    local js_file_list = { resolvePathFn('../webviews/default.js'), resolvePathFn(component_path .. '/' .. component_name .. '.js')}
+    local css_file_list = { resolvePathFn('webviews/default.css'), resolvePathFn(component_path .. '/' .. component_name .. '.css')}
+    local js_file_list = { resolvePathFn('webviews/default.js'), resolvePathFn(component_path .. '/' .. component_name .. '.js')}
 
     local absolutePathFn = g_resources.getRealPath
 
@@ -22,13 +14,13 @@ function UICEFWebView:loadComponent(component_name, component_path)
     local js_scripts = ''
 
     for _, css_file in ipairs(css_file_list) do
-        local css_path = absolutePathFn(css_file)
+        local css_path = 'file://' .. absolutePathFn(css_file)
         local css_tag = css_tag_template:gsub('{{ css_path }}', css_path)
         css_styles = css_styles .. css_tag
     end
     
     for _, js_file in ipairs(js_file_list) do
-        local js_path = absolutePathFn(js_file)
+        local js_path = 'file://' .. absolutePathFn(js_file)
         local js_tag = js_tag_template:gsub('{{ js_path }}', js_path)
         js_scripts = js_scripts .. js_tag
     end
@@ -40,5 +32,7 @@ function UICEFWebView:loadComponent(component_name, component_path)
     html = html:gsub('{{ js_scripts }}', js_scripts)
     html = html:gsub('{{ body_content }}', body_content)
 
+
+    print(html) 
     self:loadHtml(html)
 end
