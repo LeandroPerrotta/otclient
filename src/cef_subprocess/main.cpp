a5e659778bedcbccceeedc0aa65cfa870412f2f9
Leandro <leandro_perrotta@hotmail.com>
2025-08-19 19:46:48 -0300

    * Using CEF with subprocess renderer

---
+++ src/cef_subprocess/main.cpp
@@ -0,0 +1,75 @@
+#include "include/cef_app.h"
+#include "include/cef_render_process_handler.h"
+#include "include/wrapper/cef_message_router.h"
+
+#ifdef _WIN32
+#  include <windows.h>
+#endif
+
+// Render process application used in the dedicated subprocess. It mirrors the
+// behaviour that previously lived in the main executable when running in
+// single-process mode.
+class OTClientRenderApp : public CefApp, public CefRenderProcessHandler {
+public:
+    void OnRegisterCustomSchemes(CefRawPtr<CefSchemeRegistrar> registrar) override {
+        registrar->AddCustomScheme("otclient",
+                                   CEF_SCHEME_OPTION_STANDARD |
+                                   CEF_SCHEME_OPTION_LOCAL |
+                                   CEF_SCHEME_OPTION_DISPLAY_ISOLATED);
+    }
+
+    CefRefPtr<CefRenderProcessHandler> GetRenderProcessHandler() override {
+        return this;
+    }
+
+    void OnContextCreated(CefRefPtr<CefBrowser> browser,
+                          CefRefPtr<CefFrame> frame,
+                          CefRefPtr<CefV8Context> context) override {
+        if (!m_messageRouter) {
+            CefMessageRouterConfig config;
+            config.js_query_function = "luaCallback";
+            config.js_cancel_function = "luaCallbackCancel";
+            m_messageRouter = CefMessageRouterRendererSide::Create(config);
+        }
+        m_messageRouter->OnContextCreated(browser, frame, context);
+    }
+
+    void OnContextReleased(CefRefPtr<CefBrowser> browser,
+                           CefRefPtr<CefFrame> frame,
+                           CefRefPtr<CefV8Context> context) override {
+        if (m_messageRouter)
+            m_messageRouter->OnContextReleased(browser, frame, context);
+    }
+
+    bool OnProcessMessageReceived(CefRefPtr<CefBrowser> browser,
+                                  CefRefPtr<CefFrame> frame,
+                                  CefProcessId source_process,
+                                  CefRefPtr<CefProcessMessage> message) override {
+        if (m_messageRouter &&
+            m_messageRouter->OnProcessMessageReceived(browser, frame, source_process, message))
+            return true;
+        return false;
+    }
+
+private:
+    CefRefPtr<CefMessageRouterRendererSide> m_messageRouter;
+
+    IMPLEMENT_REFCOUNTING(OTClientRenderApp);
+};
+
+#if defined(_WIN32)
+// Windows entry point. Using wWinMain ensures wide-character arguments and
+// provides the HINSTANCE handle required by CefMainArgs on Windows builds.
+int APIENTRY wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR, int) {
+    CefMainArgs main_args(hInstance);
+    CefRefPtr<OTClientRenderApp> app(new OTClientRenderApp);
+    return CefExecuteProcess(main_args, app, nullptr);
+}
+#else
+int main(int argc, char* argv[]) {
+    CefMainArgs main_args(argc, argv);
+    CefRefPtr<OTClientRenderApp> app(new OTClientRenderApp);
+    return CefExecuteProcess(main_args, app, nullptr);
+}
+#endif
+
