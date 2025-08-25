bb3059cb6727eb56224bc52c0eebfa7cb27c4598
Leandro Perrotta <LeandroPerrotta@users.noreply.github.com>
2025-08-25 08:25:27 -0700

    cache memory object support

--- src/cef/graphics/gpu/cef_renderer_gpu_linux_mesa.cpp
+++ src/cef/graphics/gpu/cef_renderer_gpu_linux_mesa.cpp
@@ -11,15 +11,6 @@
 #include <thread>
 #include <cstring>
 
-#ifndef GL_EXT_memory_object_fd
-#define GL_EXT_memory_object_fd 1
-#define GL_HANDLE_TYPE_OPAQUE_FD_EXT 0x9586
-typedef void (APIENTRYP PFNGLCREATEMEMORYOBJECTSEXTPROC)(GLsizei n, GLuint* memoryObjects);
-typedef void (APIENTRYP PFNGLIMPORTMEMORYFDEXTPROC)(GLuint memory, GLuint64 size, GLenum handleType, GLint fd);
-typedef void (APIENTRYP PFNGLTEXSTORAGEMEM2DEXTPROC)(GLenum target, GLsizei levels, GLenum internalFormat, GLsizei width, GLsizei height, GLuint memory, GLuint64 offset);
-typedef void (APIENTRYP PFNGLDELETEMEMORYOBJECTSEXTPROC)(GLsizei n, const GLuint* memoryObjects);
-#endif
-
 static void* resolveGLProc(const char* name)
 {
     return (void*)glXGetProcAddressARB((const GLubyte*)name);
@@ -31,6 +22,12 @@ CefRendererGPULinuxMesa::CefRendererGPULinuxMesa(UICEFWebView& view)
     , m_textureCreated(false)
     , m_lastWidth(0)
     , m_lastHeight(0)
+    , m_checkedSupport(false)
+    , m_supported(false)
+    , m_glCreateMemoryObjectsEXT(nullptr)
+    , m_glImportMemoryFdEXT(nullptr)
+    , m_glTexStorageMem2DEXT(nullptr)
+    , m_glDeleteMemoryObjectsEXT(nullptr)
 {
     LinuxGPUContext::initialize();
 }
@@ -44,17 +41,13 @@ void CefRendererGPULinuxMesa::onPaint(const void* buffer, int width, int height,
 void CefRendererGPULinuxMesa::onAcceleratedPaint(const CefAcceleratedPaintInfo& info)
 {
 #if defined(USE_CEF) && defined(__linux__)
-    if(!LinuxGPUContext::eglSidecarReady()) {
-        g_logger.error("CefRendererGPULinuxMesa: EGL sidecar not initialized");
-        return;
-    }
     const int fd = info.planes[0].fd;
+    if(fd < 0)
+        return;
     const int width = info.extra.coded_size.width;
     const int height = info.extra.coded_size.height;
     const int stride = info.planes[0].stride;
     const int offset = info.planes[0].offset;
-    if(fd < 0)
-        return;
 
     int memFd = fcntl(fd, F_DUPFD_CLOEXEC, 0);
     if(memFd < 0)
@@ -63,14 +56,10 @@ void CefRendererGPULinuxMesa::onAcceleratedPaint(const CefAcceleratedPaintInfo& 
     g_dispatcher.addEventFromOtherThread([this, memFd, width, height, stride, offset]() mutable {
         auto close_fd = [](int& x){ if(x>=0){ ::close(x); x=-1; } };
         Display* x11Display = LinuxGPUContext::x11Display();
-        if(!x11Display) { close_fd(memFd); return; }
         if(glXGetCurrentContext() != LinuxGPUContext::mainContext()) {
             if(!glXMakeCurrent(x11Display, LinuxGPUContext::drawable(), LinuxGPUContext::mainContext())) {
                 close_fd(memFd); return; }
         }
-
-        const char* exts = (const char*)glGetString(GL_EXTENSIONS);
-        if(!exts || !strstr(exts, "GL_EXT_memory_object_fd")) { close_fd(memFd); return; }
 
         m_cefTexture = TexturePtr(new Texture(Size(width, height)));
         m_textureCreated = true;
@@ -86,33 +75,25 @@ void CefRendererGPULinuxMesa::onAcceleratedPaint(const CefAcceleratedPaintInfo& 
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_SWIZZLE_B, GL_RED);
 
         bool done = false;
-        auto glCreateMemoryObjectsEXT = (PFNGLCREATEMEMORYOBJECTSEXTPROC)resolveGLProc("glCreateMemoryObjectsEXT");
-        auto glImportMemoryFdEXT = (PFNGLIMPORTMEMORYFDEXTPROC)resolveGLProc("glImportMemoryFdEXT");
-        auto glTexStorageMem2DEXT = (PFNGLTEXSTORAGEMEM2DEXTPROC)resolveGLProc("glTexStorageMem2DEXT");
-        auto glDeleteMemoryObjectsEXT = (PFNGLDELETEMEMORYOBJECTSEXTPROC)resolveGLProc("glDeleteMemoryObjectsEXT");
-        if(glCreateMemoryObjectsEXT && glImportMemoryFdEXT && glTexStorageMem2DEXT && glDeleteMemoryObjectsEXT) {
-            GLuint memoryObject = 0;
-            glCreateMemoryObjectsEXT(1, &memoryObject);
-            if(glGetError() == GL_NO_ERROR && memoryObject != 0) {
-                GLuint64 size = (GLuint64)height * stride;
-                glImportMemoryFdEXT(memoryObject, size, GL_HANDLE_TYPE_OPAQUE_FD_EXT, memFd);
+        GLuint memoryObject = 0;
+        m_glCreateMemoryObjectsEXT(1, &memoryObject);
+        if(glGetError() == GL_NO_ERROR && memoryObject != 0) {
+            GLuint64 size = (GLuint64)height * stride;
+            m_glImportMemoryFdEXT(memoryObject, size, GL_HANDLE_TYPE_OPAQUE_FD_EXT, memFd);
+            if(glGetError() == GL_NO_ERROR) {
+                m_glTexStorageMem2DEXT(GL_TEXTURE_2D, 1, GL_RGBA8, width, height, memoryObject, offset);
                 if(glGetError() == GL_NO_ERROR) {
-                    glTexStorageMem2DEXT(GL_TEXTURE_2D, 1, GL_RGBA8, width, height, memoryObject, offset);
-                    if(glGetError() == GL_NO_ERROR) {
-                        done = true;
-                    } else {
-                        g_logger.error("CefRendererGPULinuxMesa: glTexStorageMem2DEXT failed");
-                    }
-                    memFd = -1;
+                    done = true;
                 } else {
-                    g_logger.error("CefRendererGPULinuxMesa: glImportMemoryFdEXT failed");
+                    g_logger.error("CefRendererGPULinuxMesa: glTexStorageMem2DEXT failed");
                 }
-                glDeleteMemoryObjectsEXT(1, &memoryObject);
+                memFd = -1;
             } else {
-                g_logger.error("CefRendererGPULinuxMesa: glCreateMemoryObjectsEXT failed");
+                g_logger.error("CefRendererGPULinuxMesa: glImportMemoryFdEXT failed");
             }
+            m_glDeleteMemoryObjectsEXT(1, &memoryObject);
         } else {
-            g_logger.error("CefRendererGPULinuxMesa: memory object functions missing");
+            g_logger.error("CefRendererGPULinuxMesa: glCreateMemoryObjectsEXT failed");
         }
 
         glBindTexture(GL_TEXTURE_2D, 0);
@@ -138,17 +119,37 @@ void CefRendererGPULinuxMesa::draw(Fw::DrawPane drawPane)
 bool CefRendererGPULinuxMesa::isSupported() const
 {
 #if defined(USE_CEF) && defined(__linux__)
+    if(m_checkedSupport)
+        return m_supported;
+    m_checkedSupport = true;
+
     if(!LinuxGPUContext::eglSidecarReady())
-        return false;
+        return m_supported = false;
+
     Display* x11Display = LinuxGPUContext::x11Display();
     if(!x11Display)
-        return false;
+        return m_supported = false;
+
     if(glXGetCurrentContext() != LinuxGPUContext::mainContext())
         glXMakeCurrent(x11Display, LinuxGPUContext::drawable(), LinuxGPUContext::mainContext());
+
     if(!isMesaDriver())
-        return false;
+        return m_supported = false;
+
     const char* exts = (const char*)glGetString(GL_EXTENSIONS);
-    return exts && strstr(exts, "GL_EXT_memory_object_fd");
+    if(!exts || !strstr(exts, "GL_EXT_memory_object_fd"))
+        return m_supported = false;
+
+    m_glCreateMemoryObjectsEXT = (PFNGLCREATEMEMORYOBJECTSEXTPROC)resolveGLProc("glCreateMemoryObjectsEXT");
+    m_glImportMemoryFdEXT = (PFNGLIMPORTMEMORYFDEXTPROC)resolveGLProc("glImportMemoryFdEXT");
+    m_glTexStorageMem2DEXT = (PFNGLTEXSTORAGEMEM2DEXTPROC)resolveGLProc("glTexStorageMem2DEXT");
+    m_glDeleteMemoryObjectsEXT = (PFNGLDELETEMEMORYOBJECTSEXTPROC)resolveGLProc("glDeleteMemoryObjectsEXT");
+
+    if(!m_glCreateMemoryObjectsEXT || !m_glImportMemoryFdEXT ||
+       !m_glTexStorageMem2DEXT || !m_glDeleteMemoryObjectsEXT)
+        return m_supported = false;
+
+    return m_supported = true;
 #else
     return false;
 #endif
--- src/cef/graphics/gpu/cef_renderer_gpu_linux_mesa.h
+++ src/cef/graphics/gpu/cef_renderer_gpu_linux_mesa.h
@@ -4,6 +4,19 @@
 #include "gpu_helper.h"
 #include <framework/graphics/texture.h>
 
+#if defined(USE_CEF) && defined(__linux__)
+#include <GL/gl.h>
+
+#ifndef GL_EXT_memory_object_fd
+#define GL_EXT_memory_object_fd 1
+#define GL_HANDLE_TYPE_OPAQUE_FD_EXT 0x9586
+typedef void (APIENTRYP PFNGLCREATEMEMORYOBJECTSEXTPROC)(GLsizei n, GLuint* memoryObjects);
+typedef void (APIENTRYP PFNGLIMPORTMEMORYFDEXTPROC)(GLuint memory, GLuint64 size, GLenum handleType, GLint fd);
+typedef void (APIENTRYP PFNGLTEXSTORAGEMEM2DEXTPROC)(GLenum target, GLsizei levels, GLenum internalFormat, GLsizei width, GLsizei height, GLuint memory, GLuint64 offset);
+typedef void (APIENTRYP PFNGLDELETEMEMORYOBJECTSEXTPROC)(GLsizei n, const GLuint* memoryObjects);
+#endif
+#endif
+
 class CefRendererGPULinuxMesa : public CefRenderer
 {
 public:
@@ -19,4 +32,10 @@ private:
     bool m_textureCreated;
     int m_lastWidth;
     int m_lastHeight;
+    mutable bool m_checkedSupport;
+    mutable bool m_supported;
+    PFNGLCREATEMEMORYOBJECTSEXTPROC m_glCreateMemoryObjectsEXT;
+    PFNGLIMPORTMEMORYFDEXTPROC m_glImportMemoryFdEXT;
+    PFNGLTEXSTORAGEMEM2DEXTPROC m_glTexStorageMem2DEXT;
+    PFNGLDELETEMEMORYOBJECTSEXTPROC m_glDeleteMemoryObjectsEXT;
 };
